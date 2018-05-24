06d856e2a8bb3af7b51f4ebdcae255fcd0e6590e
Cyrusp117 <30428765+Cyrusp117@users.noreply.github.com>
2018-05-25 03:09:48 +1000

    uber

--- cyrusenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cyrus\downloads\seng2021-master\cyrusenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- uber_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UberAuthConfig(AppConfig):
-    name = 'uber_auth'
